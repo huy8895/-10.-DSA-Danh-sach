be0ee080d7198aebdfacef6efe1c535fd9e60b76
huy8895 <huy8895@gmail.com>
2020-07-13 09:51:19 +0700

    [Thực hành] Triển khai lớp List đơn giản

--- src/thuchanhtrienkhailoplist/MyList.java
+++ src/thuchanhtrienkhailoplist/MyList.java
@@ -1,4 +1,31 @@
 package thuchanhtrienkhailoplist;
 
-public class MyList {
+import java.util.Arrays;
+
+public class MyList<E> {
+   public int size = 0;
+   private static final int DEFAULT_CAPACITY = 10;
+   private Object elements[];
+
+   public MyList(){
+       elements = new Object[DEFAULT_CAPACITY];
+   }
+   private void ensureCapa(){
+       int newSize = elements.length*2;
+       elements = Arrays.copyOf(elements,newSize);
+   }
+   public void add(E e){
+       if (size == elements.length){
+           ensureCapa();
+       }
+       elements[size ++] = e;
+   }
+    public E get(int i) {
+        if (i>= size || i <0) {
+            return (E) ("IndexOutOfBoundsException");
+        } else {return (E) elements[i];}
+
+
+    }
+
 }
