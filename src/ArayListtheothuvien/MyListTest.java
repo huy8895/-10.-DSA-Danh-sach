356118e0105c499a756e76763554acdd3188242f
huy8895 <huy8895@gmail.com>
2020-07-13 16:43:57 +0700

    [Bài tập] Triển khai các phương thức của ArrayList theo thư viện java.util.ArrayList

---
+++ src/ArayListtheothuvien/MyListTest.java
@@ -0,0 +1,13 @@
+package ArayListtheothuvien;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList myList = new MyList(5);
+        myList.add(0,1.1);
+        System.out.println("myList = " + myList);
+
+
+
+
+    }
+}
